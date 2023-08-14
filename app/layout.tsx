f7decbf851ac8a666b72801c2f6b9984f9eee1b0
sammykinuthia <kinuthiasamuel05@gmail.com>
2023-08-14 16:14:34 +0300

    vercel web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './components/NavBar'
 import MyFooter from './components/Footer'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,11 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <NavBar/>
+        <NavBar />
         {children}
-        <MyFooter/>
-  
-        </body>
+        <MyFooter />
+        <Analytics />
+      </body>
     </html>
   )
 }
